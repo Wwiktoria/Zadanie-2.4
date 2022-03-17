5699bcc32fc5613f3966b073f95065b27f7a491e
Wwiktoria1 <100830693+Wwiktoria@users.noreply.github.com>
2022-03-17 08:30:39 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Zadanie-24/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float main(void) {
+  float d;
+  printf("Wprowadź długość skoku:\n");
+  scanf("%f",&d);
+  float K=120;
+  float s=1.8;
+  float pd;
+  pd=60+(d-K)*s;
+
+  for (int i=0, i<5,i++);
+  
+  return 0;
+}
